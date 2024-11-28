e73dc0e57f07dda3d05898202a584fd1571172ee
MAYURIRAMNATHSHINDE <mayurishinde24304@gmail.com>
2024-11-28 18:18:46 +0530

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function signup(userName){
-    Name=["Padmavati","Mayuri","Malan"];
+    let Name=["Padmavati","Mayuri","Malan","Shree","Shrushti"];
     for(i=0;i<Name.length;i++){
         if(userName==Name[i]){
             console.log("User Already Regsistered, Please Login");
@@ -10,4 +10,22 @@ function signup(userName){
     }
 
 
+}
+
+function login(userName, password){
+    let name=["Padmavati","Mayuri","Malan","Shree","Shrushti"];
+    let validPassword="Emp@123";
+
+    for(let i=0;i<name.length;i++)
+    {
+        if(userName==name[i])
+            {
+                if (password === validPassword) {
+                    return "Login Successful...";
+                } else {
+                    return "Wrong Password...";
+                }
+            }
+    }
+    
 }
